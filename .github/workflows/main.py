b92b3ff695409df53c257bdc70f617cf1accbf6d
dhruvag1978 <112576237+dhruvag1978@users.noreply.github.com>
2023-04-26 15:10:18 +0530

    main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,17 @@
+def hello():
+    print("Hi Dhruv! Hello DevOps")
+
+    
+def bye():
+    print("Bye Dhruv")
+
+
+print(hello())def hello():
+    print("Hi Dhruv Hello DevOps")
+
+    
+def bye():
+    print("Bye Dhruv")
+
+
+print(hello())
